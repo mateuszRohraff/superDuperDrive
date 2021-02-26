ac67077d1894dff8ddc7a22f5a044cee2c675515
matirori <mateuszr600@gmail.com>
2021-02-26 17:57:22 +0100

    Notes features finished

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
@@ -16,20 +16,21 @@ public class NoteController {
         this.noteService = noteService;
     }
 
-    @PostMapping
+    @PostMapping()
     public String addNote(Note note, Authentication authentication) {
         noteService.addNote(note, authentication.getName());
         return "redirect:/home";
     }
 
-    @DeleteMapping
+    @DeleteMapping()
     public String deleteNote(@ModelAttribute Note note) {
         noteService.deleteNote(note.getNoteId());
         return "redirect:/home";
     }
 
-    @PutMapping
+    @PutMapping()
     public String updateNote(@ModelAttribute Note note, Authentication authentication) {
+        System.out.println(note.getNoteId());
         noteService.updateNote(note, authentication.getName());
         return "redirect:/home";
     }
